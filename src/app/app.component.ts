60b1157de5bc03fab544a056ea8fb9ab0170ca55
AngeloCristiano <zhou.angelo@iisgalvanimi.edu.it>
2022-03-17 08:53:24 +0000

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Empty-Project';
+
+  list: string[] = []
+  testo1:string= 'Button01';
+  testo2:string= 'Button02';
+  testo3:string= 'Button03';
+  testo4:string= 'Button04';
+
+  onClick1(){
+    this.list.push(this.testo1)
+  }
+
+  onClick2(){
+    this.list.push(this.testo2)
+  }
+
+  onClick3(){
+    this.list.push(this.testo3)
+  }
+
+  onClick4(){
+    this.list.push(this.testo4)
+  }
+  
 }
